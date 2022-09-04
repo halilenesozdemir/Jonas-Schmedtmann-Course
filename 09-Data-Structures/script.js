7310aa572ea6476c6cfb648444f6793825b88ed6
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-09-04 19:29:10 +0300

    Short Circuiting Part has finished.( && and | | )

--- 09-Data-Structures/script.js
+++ 09-Data-Structures/script.js
@@ -34,6 +34,11 @@ const restaurant = {
 
   orderPasta: function(ing1,ing2,ing3){
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients){
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   }
 
 }
@@ -137,9 +142,9 @@ const {name:restaurantName, openingHours:hours, categories: tags} = restaurant;
 
  // Mutating Variables
 
- let a = 111;
- let b = 999;
- const obj =  {a:23, b:7,c:14};
+//  let a = 111;
+//  let b = 999;
+//  const obj =  {a:23, b:7,c:14};
 
 //  {a,b} = obj;
 // Actually this usage seems logical. But we have an error .
@@ -148,8 +153,8 @@ const {name:restaurantName, openingHours:hours, categories: tags} = restaurant;
 
 //Let's solve this problem...
 // Just only a put a parenthesis :)
-  ({a,b} = obj);
-  console.log(a,b);
+  // ({a,b} = obj);
+  // console.log(a,b);
 
   // Nested Objects
 
@@ -165,18 +170,18 @@ const {name:restaurantName, openingHours:hours, categories: tags} = restaurant;
 // We use this spread operator to basically expand an array into all its elements
 // Basically unpacking all the array elements at one
 
-const arr = [7,8,9];
-const badNewArr = [1,2,arr[0],arr[1], arr[2]];
-console.log(badNewArr);
-const goodNewArr = [1,2, ...arr]
-const niceTry = [1,2,arr]
+// const arr = [7,8,9];
+// const badNewArr = [1,2,arr[0],arr[1], arr[2]];
+// console.log(badNewArr);
+// const goodNewArr = [1,2, ...arr]
+// const niceTry = [1,2,arr]
 
-console.log(goodNewArr,niceTry); // Review this results carefully.
+// console.log(goodNewArr,niceTry); // Review this results carefully.
 
-console.log(...goodNewArr);
+// console.log(...goodNewArr);
 
-const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
-console.log(newMenu);
+// const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+// console.log(newMenu);
 
 // The big difference is that the spread operator takes all the elements from the array and it also doesn't create new variables;
 //As a consequence, we can only use it in places where we would otherwise write values seperated by commas
@@ -232,6 +237,102 @@ console.log(mainMenuCopy); // SHALLOW COPY
 // console.log(restaurant.name);
 
 //SHALLOW COPY AND DEEP COPY matters will be investigated.
+
+// -----------------------------REST PATTERN AND PARAMETERS------------------------------
+//Same syntax with Spread;
+// But opposite of Spread;
+
+//Spread because on RIGHT side of =
+
+// const arr = [1,2, ...[3, 4]] ;
+
+//REST because on LEFT side of =
+const [a,b,...others] = [1,2,3,4,5];
+console.log(a,b,others); // 1 2 (3) [3, 4, 5]
+
+// REST take the rest and put them into an array :)
+
+//Both Spread and REST usage...
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza,risotto,otherFood);
+//Did you see Pasta-> NO, Because we skipped that and rest operator will be placed in at the end of array...
+
+// const [pizza, , risotto, ...otherFood,bread] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(pizza,risotto,otherFood);
+// Uncaught SyntaxError: Rest element must be last element...
+
+// Objects
+
+const {sat, ...weekDays} = restaurant.openingHours;
+console.log(weekDays);
+
+// 2)Functions
+const add =  function(...numbers){
+  let sum = 0;
+  for(let i= 0; i<numbers.length; i++){
+    sum+= numbers[i]
+  }
+  console.log(sum); 
+}
+add(2,3);
+add(5,3,7,2)
+add(8,2,5,3,2,1,4)
+
+const x = [23,5,7]
+add(...x) //Spread...
+
+restaurant.orderPizza('mushrooms','onion','olives','Spinach') // mushrooms, (3) ['onion', 'olives', 'Spinach']
+restaurant.orderPizza('mushrooms') // mushrooms, [] => empty array...
+
+// -----------------------------SHORT CIRCUITING (&& and ||) ------------------------------
+console.log('--------OR ---------');
+
+// use ANY data type, return ANY data type, short circuiting
+console.log(3 || 'Jonas'); // 3
+console.log('' || 'Jonas'); //Jonas ('' is falsy)
+console.log(true || 0); // true
+console.log(undefined || null); //null ( Both of them falsy and or operator return second.)
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null); // Hello;
+
+// restaurant.numGuests = 0;
+
+ const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('--------AND ---------');
+
+console.log(0 && 'JONAS'); // If first element is falsy, immediately return, the second not evaluated.
+console.log(7 && 'Jonas' ); // First element is truthy, it means that the evaluation continues, and then simply return the last value.
+
+console.log('Hello' && 23 && null && 'Jonas' ); // null
+
+// if(restaurant.orderPizza){
+//   restaurant.orderPizza('mushrooms', 'spinach')
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach') // Good solution, readable
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
 
 
 
