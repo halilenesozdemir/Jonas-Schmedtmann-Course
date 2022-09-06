6e0a96e26b2e992c935bbb654557ae581511a8e3
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-09-06 19:19:41 +0300

    Data Structures Part Completed(125) -> It's time to A closer look at Functions

--- 09-Data-Structures/script.js
+++ 09-Data-Structures/script.js
@@ -1186,12 +1186,12 @@ Hints:
 name conversion working 😉
 § This challenge is difficult on purpose, so start */
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
-const btn = document.querySelector('button');
-const textArea = document.querySelector('textarea');
-const texts = [];
+// const btn = document.querySelector('button');
+// const textArea = document.querySelector('textarea');
+// const texts = [];
 
 // underscore_case
 // first_name
@@ -1201,20 +1201,38 @@ const texts = [];
 // Avoid constantly copy-paste  :)
 
 
-btn.addEventListener('click',function(){
-let text= document.querySelector('textarea').value;
-const rows = text.split('\n')
-console.log(rows);
+// btn.addEventListener('click',function(){
+// let text= document.querySelector('textarea').value;
+// const rows = text.split('\n')
+// console.log(rows);
 
-for (const [index,row] of rows.entries()){
-  const [first,second] = row.toLowerCase().trim().split('_')
-  const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
-  console.log(`${output.padEnd(20)}${'✔'.repeat(index + 1)}`);
-}
+// for (const [index,row] of rows.entries()){
+//   const [first,second] = row.toLowerCase().trim().split('_')
+//   const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+//   console.log(`${output.padEnd(20)}${'✔'.repeat(index + 1)}`);
+// }
 
-})
+// })
 
     // ----------------------------- String Methods Practice  ------------------------------
+
+    const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+ 
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = (str) => str.slice(0,3).toUpperCase();
+
+console.log(flights.split('+'));
+
+for(const flight of flights.split('+')){
+  const  [type, from, to, time] = flight.split(';')
+  const output =`${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_',' ')} from ${getCode(from)} to ${getCode(to)} (${(time.replace(':','h'))})`.padStart(36)
+  console.log(output);
+}
+
 
 
 
