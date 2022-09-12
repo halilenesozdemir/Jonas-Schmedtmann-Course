be8ff89d1322919d5e61d4fa1b45167ef5da096d
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-09-12 09:09:07 +0300

    Chaining Methods and Coding Challenge #3 completed.(155-156)

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -137,101 +137,167 @@
 // BANKIST APP
 
 // Data
-// const account1 = {
-//   owner: 'Jonas Schmedtmann',
-//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-//   interestRate: 1.2, // %
-//   pin: 1111,
-// };
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
 
-// const account2 = {
-//   owner: 'Jessica Davis',
-//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: 2222,
-// };
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
 
-// const account3 = {
-//   owner: 'Steven Thomas Williams',
-//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
-//   interestRate: 0.7,
-//   pin: 3333,
-// };
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
 
-// const account4 = {
-//   owner: 'Sarah Smith',
-//   movements: [430, 1000, 700, 50, 90],
-//   interestRate: 1,
-//   pin: 4444,
-// };
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
 
-// const accounts = [account1, account2, account3, account4];
+ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// // // Elements
-// const labelWelcome = document.querySelector('.welcome');
-// const labelDate = document.querySelector('.date');
-// const labelBalance = document.querySelector('.balance__value');
-// const labelSumIn = document.querySelector('.summary__value--in');
-// const labelSumOut = document.querySelector('.summary__value--out');
-// const labelSumInterest = document.querySelector('.summary__value--interest');
-// const labelTimer = document.querySelector('.timer');
+const accounts = [account1, account2, account3, account4];
 
-// const containerApp = document.querySelector('.app');
-// const containerMovements = document.querySelector('.movements');
+// // Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
 
-// const btnLogin = document.querySelector('.login__btn');
-// const btnTransfer = document.querySelector('.form__btn--transfer');
-// const btnLoan = document.querySelector('.form__btn--loan');
-// const btnClose = document.querySelector('.form__btn--close');
-// const btnSort = document.querySelector('.btn--sort');
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
 
-// const inputLoginUsername = document.querySelector('.login__input--user');
-// const inputLoginPin = document.querySelector('.login__input--pin');
-// const inputTransferTo = document.querySelector('.form__input--to');
-// const inputTransferAmount = document.querySelector('.form__input--amount');
-// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-// const inputCloseUsername = document.querySelector('.form__input--user');
-// const inputClosePin = document.querySelector('.form__input--pin');
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
 
-// const displayMovements = function(movements){
-//   containerMovements.innerHTML = ''
-//   movements.forEach(function(mov,i,){
-//     const type = mov > 0 ? 'deposit' : 'withdrawal'
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
 
-//     const html = `
-//     <div class="movements__row">
-//       <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-//       <div class="movements__value">${mov}</div>
-//     </div>
-//     `
 
-//     containerMovements.insertAdjacentHTML('afterbegin',html)
-//     // if you use beforenend, the indexes are reversed...
+
+const displayMovements = function(movements){
+  containerMovements.innerHTML = ''
+  movements.forEach(function(mov,i,){
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+      <div class="movements__value">${mov}€</div>
+    </div>
+    `
+
+    containerMovements.insertAdjacentHTML('afterbegin',html)
+    // if you use beforenend, the indexes are reversed...
   
-// })
-// }
-// displayMovements(account1.movements)
+})
+}
+displayMovements(account1.movements)
 
-// const calcDisplayBalance = function(movements){
-//   const balance = movements.reduce((acc,mov) => acc+ mov,0);
-//   labelBalance.textContent = `${balance} EUR`
-// }
-// calcDisplayBalance(account1.movements)
+const calcDisplayBalance = function(movements){
+  const balance = movements.reduce((acc,mov) => acc+ mov,0);
+  labelBalance.textContent = `${balance} EUR`
+}
+calcDisplayBalance(account1.movements)
 
 
 
-// const createUserNames = function (accs){
-// accs.forEach(function(acc){
-//   acc.username = acc.owner
-//   .toLowerCase()
-//   .split(' ')
-//   .map((item) => item[0] )
-//   .join('')
-// });
-// };
+const createUserNames = function (accs){
+accs.forEach(function(acc){
+  acc.username = acc.owner
+  .toLowerCase()
+  .split(' ')
+  .map((item) => item[0] )
+  .join('')
+});
+};
 
-// createUserNames(accounts)
+createUserNames(accounts)
 // console.log(accounts);
+
+    // const html = `
+    // <div class="movements__row">
+    //   <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+    //   <div class="movements__value">${mov}</div>
+    // </div>
+    // `
+
+    // containerMovements.insertAdjacentHTML('afterbegin',html)
+    // // if you use beforenend, the indexes are reversed...
+
+    // Minnak Uğraşmalar
+
+// const displayDeposits = function(movements){
+//   labelSumIn.innerHTML = '';
+//  const sumOfDeposits = movements.filter((mov) => mov > 0 )
+//  .reduce((acc,mov) =>  acc + mov,0)
+//    labelSumIn.innerHTML = `${sumOfDeposits}€ `
+// }
+// // const deposits = displayDeposits(movements)
+// // console.log(deposits);
+// displayDeposits([200, 450, -400, 3000, -650, -130, 70, 1300])
+
+// const displayWithDrawal = function(movements){
+//   labelSumOut.innerHTML = '';
+//  const sumOfDeposits = movements.filter((mov) => mov < 0 )
+//  .reduce((acc,mov) =>  acc + mov,0)
+//    labelSumOut.innerHTML = `${sumOfDeposits}€ `
+// }
+// // const deposits = displayDeposits(movements)
+// // console.log(deposits);
+// displayDeposits([200, 450, -400, 3000, -650, -130, 70, 1300])
+// displayWithDrawal([200, 450, -400, 3000, -650, -130, 70, 1300])
+
+
+// const calcDisplaySummary = function(movements){
+//   const incomes = movements.filter(mov => mov > 0)
+//   .reduce((acc,mov)=> acc + mov,0);
+//   labelSumIn.textContent = `${incomes} € `
+
+//   const out = movements.filter(mov => mov < 0)
+//   .reduce((acc,mov)=> acc + mov,0)
+//   labelSumOut.textContent = `${Math.abs(out)} € `
+
+//   const interest = movements.filter(mov => mov > 0)
+//   .map((deposit) => deposit * 1.2/100)
+//   .filter((int,i,arr) => {
+//     console.log(arr);
+//     return int >=1;
+//   })
+//   .reduce((acc,int)=> acc + int,0);
+//   labelSumInterest.textContent =  `${interest}€`
+// }
+
+
+
+
+// calcDisplaySummary(account1.movements)
+
+
+
+
 
 
 
@@ -389,6 +455,24 @@ Just wanted to share, maybe someone can benefit.
 
 // console.log(max);
 
+// ---------------------- The Magic of Chaining Methods  ----------------------
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//  const eurToUsd= 1.1;
+// // PIPELINE
+// const sumOfEuro = movements.filter((mov) => mov > 0 )
+// // .map((mov) => mov * eurToUsd)
+// .map((mov ,i ,arr) =>{
+//   // console.log(arr);
+// return mov * eurToUsd
+// } )
+
+// .reduce((acc,mov) => acc + mov,0 );
+
+
+// console.log(sumOfEuro);
+
+
 // ---------------------- Coding Challenge #2  ----------------------
 
 // Let's go back to Julia and Kate's study about dogs. This time, they want to convert
@@ -451,15 +535,75 @@ Just wanted to share, maybe someone can benefit.
 // const avg2= calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
 // console.log(avg1,avg2);
 
-const calcAverageHumanAge = function (ages) {
-  const humanAge = ages
-    .map(ages => (ages <= 2 ? (ages = 2 * ages) : (ages = 16 + ages * 4)))
-    .filter(ages => ages > 18)
-    .reduce((acc, ages, _, arr) => acc + ages / arr.length, 0);
-  return humanAge;
-};
-console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
-console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+// const calcAverageHumanAge = function (ages) {
+//   const humanAge = ages
+//     .map(ages => (ages <= 2 ? (ages = 2 * ages) : (ages = 16 + ages * 4)))
+//     .filter(ages => ages > 18)
+//     .reduce((acc, ages, _, arr) => acc + ages / arr.length, 0);
+//   return humanAge;
+// };
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+
+// ---------------------- Coding Challenge #3  ----------------------
+
+// Let's go back to Julia and Kate's study about dogs. This time, they want to convert
+// dog ages to human ages and calculate the average age of the dogs in their study.
+// Your tasks:
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
+// ages ('ages'), and does the following things in order:
+// 1. Calculate the dog age in human years using the following formula: if the dog is
+// <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
+// humanAge = 16 + dogAge * 4
+// 2. Exclude all dogs that are less than 18 human years old (which is the same as
+// keeping dogs that are at least 18 years old)
+// 3. Calculate the average human age of all adult dogs (you should already know
+// from other challenges how we calculate averages 😉)
+// 4. Run the function for both test datasets
+// Test data:
+// § Data 1: [5, 2, 4, 1, 15, 8, 3]
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+// Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+// as an arrow function, and using chaining!
+// Test data:
+
+// const calcAverageHumanAge = function(ages){
+// const humanAges = ages.map((age) => (age <=2) ? age * 2 : 16 + age*4 )
+// .filter((age) => age > 18 )
+// .reduce((acc,age,i,arr) => acc + age /arr.length,0)
+// return humanAges
+// // Bu metodda adults.length yaklaşımı geçersizdir....
+
+// }
+
+// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+const calcAverageHumanAge = function(dogAge){
+  const x = dogAge.map((cur)=>{
+ if (cur <=2 ){
+      return cur * 2
+    }else {
+      return 16 + cur * 4 
+    }
+  })
+  .filter((cur)=>{
+    if (cur >= 18) {
+      return cur
+    }
+  }).reduce((acc, cur, i, arr)=>{
+    return acc+ cur /arr.length
+  }, 0)
+  return x
+}
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]))
+
+
+
+
+
+
 
 
 
