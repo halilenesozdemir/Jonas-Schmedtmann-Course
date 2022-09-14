8a651a18a4f1e924de591e25ab0c838525b872d6
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-09-14 14:56:10 +0300

    Creating and filling arrays part completed(164)

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -814,3 +814,43 @@ console.log(everyFunction(account4.movements)); */
 // console.log(movements);
 
 //Please don't this sort method mixing cases. (Contains String + Numbers)
+
+// ---------164. More Ways of Creating and Filling Arrays-------
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+//// Empty arrays + fill method
+// const x = new Array(7);
+// console.log(x);
+// console.log(x.map(() => 5));
+
+// x.fill(1, 3, 5);
+// console.log(x);
+
+// arr.fill(23, 2, 6);
+// console.log(arr);
+
+// Array.from
+// const y = Array.from({ length: 7 }, () => 1);
+// console.log(y);
+
+// const z = Array.from({ length: 7 }, (_, i) => i + 1);
+// console.log(z);
+
+//Diceroll example
+
+// const diceRoll = Array.from({ length: 100 }, () => Math.trunc(Math.random() * 6) + 1);
+// console.log(diceRoll);
+
+labelBalance.addEventListener('click', function () {
+  //We prefer this from nodelist (dom) to array
+  const movementsUI = Array.from(document.querySelectorAll('.movements__value'), (el) =>
+    Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+
+  // const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  // console.log(movementsUI2);
+});
