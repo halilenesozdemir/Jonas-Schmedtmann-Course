1ab026c28b9d7a03b2f4fba883822e685e04f2c0
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-09-15 11:38:12 +0300

    Numeric Separators part handled (173)

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -393,6 +393,34 @@ labelBalance.addEventListener('click', function(){
 )})
 // hearing Nth time -> Good feature -> remainder
 
+// -------------------- Numeric Separators ---------------
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415
+console.log(PI);
+
+console.log(Number('230000')); // It worked
+console.log(Number('230_000')); // NaN
+
+console.log(parseInt('230_000')); // 230 -> In front of the underscore
+
+
+
+
+
+
+
+
+
 
 
 
