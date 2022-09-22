80599f110c7b856326a66cb96f2f4b3895fa9abd
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-09-22 19:13:29 +0300

    Event Delegation part handled(192)

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -2,33 +2,84 @@
 
 ///////////////////////////////////////
 // Modal window
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-// const modal = document.querySelector('.modal');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal = document.querySelector('.btn--close-modal');
-// const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-// const openModal = function (e) {
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+//////////////////////////////////////////////
+// Button scrolling
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect()); // basically relative your viewport
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log('height/width', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  Scrolling;
+  window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+////////////////////////////////////////////////
+//Page Navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+// el.addEventListener('click', function (e) {
 //   e.preventDefault();
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-// document.addEventListener('keydown', function (e) {
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
+//     const id = this.getAttribute('href');
+//     // console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
 // });
+
+// 1. Add event listener to common parent element
+// 2.Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
 // Selecting Elements
 // Generally we use dqs, dqsa with nodelist...
 // console.log(document.documentElement);
@@ -120,32 +171,32 @@ console.log(link.getAttribute('href'));
 // Don't use
 // logo.className = 'jonas';
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect()); // basically relative your viewport
+// console.log(e.target.getBoundingClientRect()); // basically relative your viewport
 
-  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+// console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
-  // console.log('height/width', document.documentElement.clientHeight, document.documentElement.clientWidth);
+// console.log('height/width', document.documentElement.clientHeight, document.documentElement.clientWidth);
 
-  // Scrolling
-  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+// Scrolling
+// window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
 
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
+// window.scrollTo({
+//   left: s1coords.left + window.pageXOffset,
+//   top: s1coords.top + window.pageYOffset,
+//   behavior: 'smooth',
+// });
 
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
 // const alertH1 = function (e) {
 //   alert('addEventListener: Great! You are reading the heading :)');
@@ -172,30 +223,30 @@ const h1 = document.querySelector('h1');
 
 //----------------------- Event Propagation in Practice---------------
 
-//rgb(255,255,255);
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-console.log(randomColor());
+//  rgb(255,255,255);
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// console.log(randomColor());
 
-document.querySelector('.nav__link').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log('LINK', e.target, e.currentTarget);
-  console.log(e.currentTarget === this);
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('LINK', e.target, e.currentTarget);
+//   console.log(e.currentTarget === this);
 
-  // STOP PROPAGATION
-  // e.stopPropagation();
-});
+//   // STOP PROPAGATION
+//   // e.stopPropagation();
+// });
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log('container', e.target, e.currentTarget);
-});
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('container', e.target, e.currentTarget);
+// });
 
-document.querySelector('.nav').addEventListener(
-  'click',
-  function (e) {
-    this.style.backgroundColor = randomColor();
-    console.log('nav', e.target, e.currentTarget);
-  },
-  false // see the capturing phase, by default third parameter is false.
-);
+// document.querySelector('.nav').addEventListener(
+//   'click',
+//   function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('nav', e.target, e.currentTarget);
+//   },
+//   false // see the capturing phase, by default third parameter is false.
+// );
