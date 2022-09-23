435dc81de64bdc05c14b23bd5953952f9f4e4824
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-09-23 07:46:14 +0300

    Dom traversing part handled. (193)

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -250,3 +250,33 @@ console.log(link.getAttribute('href'));
 //   },
 //   false // see the capturing phase, by default third parameter is false.
 // );
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'black';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children); // All of them siblings...
+
+//A bit of fun :)
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = 'scale(1)';
+// });
